5f9fceb38614132e4dc88a6ca11d73905bb97d86
Bhargav-Techin <148316999+Bhargav-Techin@users.noreply.github.com>
2024-07-28 12:23:33 +0530

    add register controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,72 @@
-import { asyncHandler } from "../utils/asyncHandler.js"
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { apiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
-const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "User registered successfully"
-    })
-})
+const registerUser = asyncHandler(async (req, res) => {
+  // get user details from frontend
+  //validation -- not empty
+  // check if user already exist -- email and username
+  // check or images and avatar
+  //upload them to cloudinary, check for avatar
+  //create user object -- create entry in db
+  // remove password and refresh token feild from response
+  // check for user creation
+  // return response
 
-export { registerUser }
+  const { fullname, email, username, password } = req.body;
+  console.log("email: ", email);
+
+  if (
+    [fullname, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new apiError(400, "All fields are required");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ email }, { username }],
+  });
+
+  if (existedUser) {
+    throw new apiError(409, "Email or username already taken");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new apiError(400, "Please upload avatar");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new apiError(500, "Failed to upload avatar image to cloudinary");
+  }
+
+  const user = await User.create({
+    fullname,
+    email,
+    username: username.lowercase,
+    password,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new apiError(500, "Failed to create user");
+  }
+
+  return res.status(201).json(
+    new ApiResponse(200, createdUser, "User registered successfully")
+  )
+
+});
+
+export { registerUser };
